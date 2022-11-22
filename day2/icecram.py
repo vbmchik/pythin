92cd413a811bae99c73cc0f69af6ade331dab637
vbmchik <vbmchik@gmail.com>
2022-11-22 20:27:46 +0200

    pdf add

---
+++ day2/icecram.py
@@ -0,0 +1,4 @@
+    
+    
+machine = IceCreamMachine(["vanilla", "chocolate"], ["chocolate sauce", "liquor"])
+print(machine.scoops()) #should print: [['vanilla', 'chocolate sauce'], ['chocolate', 'chocolate sauce']]
