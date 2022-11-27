eb970abeddb5dba5850ca579156ce9cede830c7f
vbmchik <vbmchik@gmail.com>
2022-11-27 20:41:56 +0200

    fix

--- day2/hometask/task1.py
+++ day2/hometask/task1.py
@@ -1,4 +1,40 @@
+import copy
 
+def addPhrase(beasts, name, phrase):
+    beast = getBeast(beasts, name)
+    beast['Фраза'] = phrase
+
+def addBeast(beasts, name, color, series ):
+    beasts.append({
+        "Имя" : name,
+        "Цвет" : color, 
+        "Серии" : series
+    })
+def findBeast(beasts, name):
+    n = -1
+    for i in [0,len(beasts)-1, 1]:
+        if beasts[i]['Имя'] == name:
+           n = i
+           return n
+    return n       
+
+def delBeast(beasts, name):
+    n = findBeast(beasts,name)
+    if n == -1 :
+        print('No beast')
+        return
+    beasts.pop(n)
+
+def allSeries(beasts):
+    l = []
+    for beast in beasts:
+        l += beast['Серии']
+    f = filter(lambda x: l.count(x)>1, l)
+    print(set(f))    
+
+def getBeast(beasts, name):
+    l = filter( lambda b: b["Имя"] == name, beasts)
+    return (list(l)[0])
 # Здесь должен быть ваш код - методы которые требуются в задаче мы не используем if, for, while... 
 
 #
@@ -19,21 +55,32 @@ beasts.append({
 
 # нельзя использовать циклы и условия
 #Добавление любимых фраз Копатыча и Лосяша
-addPhrase(beasts, "Лосяш", "Хрю")
+#addPhrase(beasts, "Лосяш", "Хрю")
+#print(beasts)
 #Поиск серий, в которых участвуют оба персонажа
-print(seriesSets(beasts))
+#allSeries(beasts)
+#print(seriesSets(beasts))
 #Удаление серии Герой Плутона из списка серий Лосяша
-seriesRemove(beasts, 'Лосяш', 'Герой Плутона')
-print(getBeast(beasts,'Лосяш'))
+#l = getBeast(beasts,'Лосяш')
+#l['Серии'].remove('Герой Плутона')
+#print(l)
+#seriesRemove(beasts, 'Лосяш', 'Герой Плутона')
+#print(getBeast(beasts,'Лосяш'))
 #Замена цвета Копатыча на Светло-коричневый
-changeColor(beasts,"Копатыч","Светло-коричневый")
-print(getBeast(beasts,'Копатыч'))
+#l = getBeast(beasts,'Копатыч')
+#l['Цвет'] = "Светло-коричневый"
+#print(l)
+#changeColor(beasts,"Копатыч","Светло-коричневый")
+#print(getBeast(beasts,'Копатыч'))
 #Добавление данных о любом другом смешарике пользователем
-addBeast(beasts)
-print(beasts)
+#addBeast(beasts)
+#print(beasts)
 #Подсчет количества записанных смешариков
 #???
 #Создание второй структуры, идентичной первой, удаление оттуда записи про Копатыча так чтобы оригинальная структура не поменялась
 # Создавайте )
-deleteBeast(omg,'Копатыч')
-print(omg)
+#deleteBeast(omg,'Копатыч')
+beasts2 = beasts.copy()
+delBeast(beasts2,'Копатыч')
+print(beasts)
+print(beasts2)
