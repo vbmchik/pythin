e850b201ea7f1ba53d86e63753d006c43210495d
vbmchik <vbmchik@gmail.com>
2022-11-27 19:56:50 +0200

    mega super hard

--- day2/hometask/icecram.py
+++ day2/hometask/icecram.py
@@ -1,4 +1,25 @@
+import copy
+
+def combine(files, n, pres):
+    flag = n == len(files)-1
+    for x in files[n]:
+        res = copy.copy(pres)
+        res.append(x)
+        if flag:
+            print(res)
+        else:
+            combine(files,n+1,res)    
     
-    
-machine = IceCreamMachine(["vanilla", "chocolate"], ["chocolate sauce", "liquor"])
-print(machine.scoops()) #should print: [['vanilla', 'chocolate sauce'], ['chocolate', 'chocolate sauce']]
+def ice(icecream):
+    for i in [0,1]:
+        for j in [0,1]:
+            print(icecream[0][i],icecream[1][j])
+
+machine = [["vanilla", "chocolate"], ["chocolate sauce", "liquor"]]
+#           1 .                 2     
+# 1 .   ["vanilla",         "chocolate"]
+# 2 .   ["chocolate sauce", "liquor"]
+# .   1,1 - 1,2 - 2,1 - 2,2
+# .   0,0 - 0,1 - 1,0 - 1,1
+ice(machine)
+#combine(machine,0,[])
--- day2/hometask/palindrom.py
+++ day2/hometask/palindrom.py
@@ -1,4 +1,12 @@
 
+def palindrom(str):
+    str = str.lower().replace(' ', '')
+    return str == str[::-1]
+
 
 #напишите метод (функцию) которая будет проверять является ли строка палиндромом (совпадает с собой в перевернутом виде)
-string = "Let it itel"
+s = "Let it itel"
+q = lambda x : x.lower().replace(' ','')
+t = q(s) == q(s)[::-1]
+print(t)
+#print(palindrom(s))
