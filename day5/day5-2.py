0647bad1cfe3011c5d5966dc366553b3cfb8f600
vbmchik <vbmchik@gmail.com>
2022-12-04 20:36:49 +0200

    bow bow

---
+++ day5/day5-2.py
@@ -0,0 +1,16 @@
+class Dog():
+    def __init__(self, name, age) :
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print(f"{self.name} barks - bow, bow, bow!")
+    
+    # спец метод у каждого класса он возвращает строку 
+    def __str__(self):
+        return f"dog {self.name} of age {self.age}"
+
+dog = Dog('Snoopy', 12)
+s = str(dog)
+dog.bark()
+print(s)
