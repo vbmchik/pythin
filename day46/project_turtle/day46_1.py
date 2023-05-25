a0e9d0dd7db09eb1e77af9ce1f446f96a8f5e116
vbmchik <vbmchik@gmail.com>
2023-05-25 08:15:40 +0300

    fix

--- day46/project_turtle/day46_1.py
+++ day46/project_turtle/day46_1.py
@@ -14,8 +14,9 @@ turtle1 = Turtle(b, (14,16), 2)
 turtle2 = Turtle(b, (4, 16), 3)
 turtle3 = Turtle(b, (14, 8), 4)
 turtle4 = Turtle(b, (8, 17), 5)
+turtle5 = Turtle(b, (19, 2), 6)
 os.system('clear')
-while (any([turtle.move(), turtle1.move(), turtle2.move(), turtle3.move(), turtle4.move()])):
+while (any([turtle.move(), turtle1.move(), turtle2.move(), turtle3.move(), turtle4.move(), turtle5.move()])):
     pprint(b.board)
     time.sleep(0.5)
     os.system('clear')
--- day46/project_turtle/turtle.py
+++ day46/project_turtle/turtle.py
@@ -8,32 +8,44 @@ class Turtle:
         self.board = board
         self.__point = point
         self.__vector = 0
+        self.board.board = point[0], point[1], value
         self.camove=True
         self.__value = value
         
     def pointsum(self, point1: tuple, point2:tuple):
         return (point1[0]+point2[0], point1[1]+point2[1])
 
+    def leftvec(self):
+        return (-1*self.__vectors[self.__vector][1],
+                self.__vectors[self.__vector][0])
+    
     def leftcell(self, x, y):
-        pvector = (-1*self.__vectors[self.__vector][1],
-                   self.__vectors[self.__vector][0])
-        ppoint = self.pointsum((x, y), pvector)
+        ppoint = self.pointsum((x, y), self.leftvec())
         return self.board.freecell(ppoint[0], ppoint[1])
     
+    def turnvector(self):
+        if self.__vector == 3:
+            self.__vector = 0
+        else:
+            self.__vector = self.__vector + 1
+            
+    def insideboard(self, x, y):
+            return x <= self.board.n and y <= self.board.n and x >= 1 and y >= 1
+        
     def validate(self, x, y):
-        if x <= self.board.n and y <= self.board.n and x >= 0 and y >= 0:
+        if self.insideboard(x,y):
             if self.board.freecell(x, y):
                 self.board.board = x, y, self.__value
                 self.__point = (x, y)
                 if not self.leftcell(x, y):
                     return True
-                if self.__vector == 3:
-                    self.__vector = 0
-                else:
-                    self.__vector = self.__vector + 1
+                self.turnvector()
                 return True
         return False
 
+    def onborderline(self, x, y):
+        return x == 1 or y == 1 or x == self.board.n or y == self.board.n
+    
     def move(self):
         if not self.camove:
             return False
@@ -44,7 +56,7 @@ class Turtle:
                 k = self.__vector + i
             x, y = self.pointsum(self.__vectors[k], self.__point)
             if self.validate(x, y):
-                if x == 1 or y == 1 or x == self.board.n or y == self.board.n:
+                if self.onborderline(x,y):
                     if not self.leftcell(x,y):
                         a, b = self.pointsum(
                             self.__vectors[self.__vector], (x, y))
