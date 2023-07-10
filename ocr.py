b0026c89b181428bade936d2b9c18c1e830121f2
vbmchik <vbmchik@gmail.com>
2023-07-10 06:48:55 +0300

    nexr

--- ocr.py
+++ ocr.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 # [ x**2 for x in [1, 2, 3] ]
 images = [
-    keras_ocr.tools.read(img) for img in [  "1.jpg", ]
+    keras_ocr.tools.read(img) for img in [  "card1.png", ]
 ]
 pipeline = keras_ocr.pipeline.Pipeline()
 prediction_groups = pipeline.recognize(images)
-fig, axs = plt.subplots(nrows=len(images), figsize=(10, 20))
-for ax, image, predictions in zip(axs, images, prediction_groups):
-    keras_ocr.tools.drawAnnotations(image=image,
-                                    predictions=predictions,
-                                    ax=ax)
-predicted_image = prediction_groups[1]
+#fig, axs = plt.subplots(nrows=len(images), figsize=(10, 20))
+#for ax, image, predictions in zip(axs, images, prediction_groups):
+#    keras_ocr.tools.drawAnnotations(image=image,
+#                                    predictions=predictions,
+#                                    ax=ax)
+predicted_image = prediction_groups[0]
 for text, box in predicted_image:
     print(text)
